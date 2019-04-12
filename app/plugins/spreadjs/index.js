cfddf1c3610f06b55b2dafa5cc39916cd3070623
Evolvan <evolvan@MBP-15.fritz.box>
2019-04-12 13:21:45 +0530

    spreadsheet section changes

--- app/plugins/spreadjs/index.js
+++ app/plugins/spreadjs/index.js
@@ -4,11 +4,13 @@ var onLoadInitializeSpreadSheet = function (id, data) {
     var spread = new gc.Spread.Sheets.Workbook(document.getElementById(id));
     try {
         spread.fromJSON(JSON.parse(data));
+        spread.getActiveSheet().clearSelection();
         
         setTimeout(() => {
             spread.suspendPaint();
             spread.getActiveSheet().options.isProtected = true;
             spread.getActiveSheet().getRange(0, 0, spread.getActiveSheet().getRowCount(), spread.getActiveSheet().getColumnCount(), gc.Spread.Sheets.SheetArea.viewport).locked(true);
+            spread.getActiveSheet().clearSelection()
             spread.resumePaint();
         }, 500);
     } catch(e) {
